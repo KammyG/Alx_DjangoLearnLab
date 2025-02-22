a9941eed7899284656285647074886785a50e6b6
kami <muriukichristiana@gmail.com>
2025-02-22 17:00:28 +0300

    Fixed get_books_by_author to use Author.objects.get() as required

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -7,11 +7,13 @@ django.setup()
 from relationship_app.models import Author, Book, Library, Librarian
 
 def get_books_by_author(author_name):
-    author = Author.objects.filter(name=author_name).first()
-    if author:
+    try:
+        author = Author.objects.get(name=author_name)  # Use .get() instead of .filter().first()
         books = Book.objects.filter(author=author)
         return [book.title for book in books]
-    return []
+    except Author.DoesNotExist:
+        return []
+
 
 def get_books_in_library(library_name):
     try:
