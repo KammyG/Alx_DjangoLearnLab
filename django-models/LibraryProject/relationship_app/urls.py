9d29bb52179fc8f19896852affbd3ca524e34451
kami <muriukichristiana@gmail.com>
2025-02-23 17:02:09 +0300

    Fixed authentication URL patterns using built-in LoginView and LogoutView

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,18 +1,14 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
+
 
 urlpatterns = [
-    # Authentication URLs (Choose Either CustomLoginView or user_login)
     path("register/", views.register, name="register"),
-    path("login/", views.user_login, name="login"),  # Using function-based login
-    path("logout/", views.user_logout, name="logout"),
+    path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
 
-    # Homepage
-    path("", views.home, name="home"),
-
-    # Books List
-    path("books/", views.list_books, name="list_books"),
-
-    # Library Detail View
-    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
+    path('', views.home, name='home'),  
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
