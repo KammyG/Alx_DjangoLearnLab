a655e5d39c8a656539496a2060ff0e947518ae3a
kami <muriukichristiana@gmail.com>
2025-02-23 16:13:20 +0300

    Added authentication views for login, logout, and registration

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,11 +2,17 @@ from django.urls import path
 from . import views
 from .views import list_books, LibraryDetailView
 from .views import register, CustomLoginView, CustomLogoutView
+from .views import register, user_login, user_logout
 
 urlpatterns = [
     path("register/", register, name="register"),
     path("login/", CustomLoginView.as_view(), name="login"),
     path("logout/", CustomLogoutView.as_view(), name="logout"),
+
+    path("register/", register, name="register"),
+    path("login/", user_login, name="login"),
+    path("logout/", user_logout, name="logout"),
+
     
     path('', views.home, name='home'),  # Homepage URL
     path('books/', views.list_books, name='list_books'),
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from .models import Book, Library
 from django.contrib.auth import login, logout
-from django.contrib.auth.forms import  UserCreationForm
+from django.contrib.auth.forms import  UserCreationForm, AuthenticationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.models import User
@@ -12,16 +12,35 @@ from django import forms
 
 def home(request):
     return render(request, 'relationship_app/home.html')
+
 def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)
-            return redirect("login")  # Redirect to book list or home page
+            login(request, user)  # Automatically log in the user after registration
+            return redirect("home")  # Redirect to the home page or another page
     else:
         form = UserCreationForm()
-    return render(request, "relationship_app/register.html", {"form": form})
+    
+    return render(request, "register.html", {"form": form})
+
+def user_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, "login.html", {"form": form})
+
+def user_logout(request):
+    logout(request)
+    return redirect("home")  # Redirect to home after logout
+
 
 # Custom Login View
 class CustomLoginView(LoginView):
