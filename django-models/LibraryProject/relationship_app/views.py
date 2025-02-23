5f7bb10e819518a4b8364b1c9f68de466ce11e97
kami <muriukichristiana@gmail.com>
2025-02-23 15:40:41 +0300

    Fixed import for UserCreationForm in views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from .models import Book, Library
 from django.contrib.auth import login, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import  UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView, LogoutView
 
