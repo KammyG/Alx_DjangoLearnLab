2e0e3de8bdfa9eb3814326ee6cf396a158eebd55
kami <muriukichristiana@gmail.com>
2025-03-04 15:34:15 +0300

    created serializers.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from .models import Book
+from .serializers import BookSerializer
+from django.http import JsonResponse
 
-# Create your views here.
+def home(request):
+    return JsonResponse({"message": "Welcome to the API!"})
+
+class BookList(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
