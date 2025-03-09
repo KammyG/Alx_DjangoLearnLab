f1d8a6a4f95bb5951501376829dddab9f0b6c598
kami <muriukichristiana@gmail.com>
2025-03-09 15:23:06 +0300

    updated views.py to include generics.ListAPIView

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,15 +1,15 @@
 from django.http import JsonResponse
-from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .permissions import IsAdminOrReadOnly
+from rest_framework import generics
 
 def home(request):
     return JsonResponse({"message": "Welcome to the API!"})
 
-class BookList(ListAPIView): 
+class BookList(generics.ListAPIView): 
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
