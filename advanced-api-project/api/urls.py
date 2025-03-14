f01056579f902ebf5bb71f16ee5894caabf19f81
kami <muriukichristiana@gmail.com>
2025-03-14 21:10:37 +0300

    updated api/urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
 
 urlpatterns = [
-    path('', BookListView.as_view(), name='book-list'),
-    path('<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-    path('create/', BookCreateView.as_view(), name='book-create'),
-    path('<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
-    path('<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+    path('books/', BookListView.as_view(), name='book-list'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('books/create/', BookCreateView.as_view(), name='book-create'),
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
 ]
+
